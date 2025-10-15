0acf570f50096b01a39eb60f69123b9f71609c0e
AVIASOLE\aviasole <deep@nextere.com>
2025-10-15 16:43:00 +0530

    changes in api loading

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,17 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig((command, mode) => {
-  const env = loadEnv(mode, process.cwd(), "")
-
-  console.log(env.VITE_DEBUG)
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
-    ...(env.VITE_DEBUG === "true" && {
       proxy: {
         "/api": {
           target: "https://choose-your-own-adventure-be.onrender.com",
           changeOrigin: true,
-          secure: false
-        }
-      }
-    }
-    )}
-  }
-})
+          secure: false,
+        },
+      },
+    },
+  };
+});
