b979625f207dac138276a3797822103c003fabcc
AVIASOLE\aviasole <deep@nextere.com>
2025-10-15 17:50:56 +0530

    changes in api exchange

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://choose-your-own-adventure-be.onrender.com",
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-    preview: {
-      host: true,
-      allowedHosts: "all",
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: "all",
+  },
 });
